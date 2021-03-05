575892edf18eb45c5666aceda98ada8b82fd0236
Łukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-03-05 17:14:23 +0000

    This is not allowed on RDS
    Signed-off-by: Łukasz Gryglicki <lukaszgryglicki@o2.pl>

--- mariadb_prod2test.sh
+++ mariadb_prod2test.sh
@@ -34,8 +34,8 @@ echo 'dumping from prod'
 > "${fn}"
 if [ -z "${SKIP_TX}" ]
 then
-  echo "SET SESSION innodb_lock_wait_timeout=1800;" >> "${fn}"
-  echo "SET GLOBAL innodb_lock_wait_timeout=1800;" >> "${fn}"
+  #echo "SET SESSION innodb_lock_wait_timeout=1800;" >> "${fn}"
+  #echo "SET GLOBAL innodb_lock_wait_timeout=1800;" >> "${fn}"
   echo "BEGIN;" >> "${fn}"
 fi
 echo "DELETE FROM changes_cache;" >> "${fn}"
@@ -56,7 +56,7 @@ echo "DELETE FROM changes_cache;" >> "${fn}"
 if [ -z "${SKIP_TX}" ]
 then
   echo "COMMIT;" >> "${fn}"
-  echo "SET GLOBAL innodb_lock_wait_timeout=120;" >> "${fn}"
+  #echo "SET GLOBAL innodb_lock_wait_timeout=120;" >> "${fn}"
 fi
 date
 echo 'restoring to test'
